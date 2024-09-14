2800be29080c8d604b66c7ccc894d149e81c1a05
jason <jason@soui.com.tw>
2024-09-14 11:17:31 +0800

    while forz範例

--- lesson01.py
+++ lesson01.py
@@ -1,2 +1,17 @@
-x = 'Hello Python!!'
-print(x)
+# 計算1到100的總和
+total = 0
+for i in range(1, 101):
+    total += i
+print(total)
+
+
+# 使用while迴圈計算1到100的總和
+total = 0
+counter = 1
+
+while counter <= 100:
+    total += counter
+    counter += 1
+
+print(f"使用while迴圈計算的1到100總和為: {total}")
+
