4d6cc1ac3de0d86f426793323add3fe59b36772d
黃俊傑 <33945741+jasonSOUI@users.noreply.github.com>
2024-09-21 14:13:58 +0800

    timer

--- pico/leasson4/timer.py
+++ pico/leasson4/timer.py
@@ -1,9 +1,21 @@
 from machine import Timer
+from machine import Pin
 
-def hello(t):
-    print(time.freq)
+count = 0
+led = Pin("LED", Pin.OUT)
+
+# LED閃爍
+def toggle_led(t:Timer):
+    global count
+    count = count + 1
+    led.toggle()
+    print(f"toggle_led目前被執行:{count}次")
+    if count >= 10:
+        t.deinit()
+        print("結束執行")
     
-tim = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
-tim.init(period=2000, mode=Timer.PERIODIC, callback=hello)
+
+# 定時器
+led_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=toggle_led)
 
 
